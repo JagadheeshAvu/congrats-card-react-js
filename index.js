e4b62a221c0c55a5b059b5da0c0b590c45868b9e
JagadheeshAvu <jagadheesh6349@gmail.com>
2023-03-10 11:43:53 +0000

    congrats card react js

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="congratulate-heading">Congratulations</h1>
+    <div className="inner-bg">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="img"
+      />
+      <h1 className="name">Kiran V</h1>
+      <p className="description">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
